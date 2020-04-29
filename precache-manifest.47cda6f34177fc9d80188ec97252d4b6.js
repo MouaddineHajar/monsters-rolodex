cd5981ff5e60f2461e3c0523dec89a64642d7961
Hajar Mouaddine <ag18748@LC02ZX1P3MD6R.US.AD.WELLPOINT.COM>
2020-04-29 00:10:48 -0400

    Updates

---
+++ precache-manifest.47cda6f34177fc9d80188ec97252d4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c47e8aec03150a5859bf13f96b099fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63578189d760df814bb2",
+    "url": "/monsters-rolodex/static/css/main.599c5941.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63578189d760df814bb2",
+    "url": "/monsters-rolodex/static/js/main.749e4666.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
